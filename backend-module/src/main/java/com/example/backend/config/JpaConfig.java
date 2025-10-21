52c9c6bba945887d2064fb9a6a15ebdc64f895d6
ortzion-gabrielsousa <gabriel.sousa@ortzion.com.br>
2025-10-21 12:08:52 -0300

    correcao

--- backend-module/src/main/java/com/example/backend/config/JpaConfig.java
+++ backend-module/src/main/java/com/example/backend/config/JpaConfig.java
@@ -1,25 +1,36 @@
 package com.example.backend.config;
 
 import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.PersistenceUnit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jndi.JndiObjectFactoryBean;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.naming.NamingException;
 
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = "com.example.backend")
 public class JpaConfig {
 
-    @PersistenceUnit(unitName = "default")
-    private EntityManagerFactory emf;
-
     @Bean
-    public PlatformTransactionManager transactionManager() {
-        return new JpaTransactionManager(this.emf);
+    public JndiObjectFactoryBean entityManagerFactoryLookup() {
+        JndiObjectFactoryBean factoryBean = new JndiObjectFactoryBean();
+        factoryBean.setJndiName("java:jboss/EntityManagerFactory/ear-module-0.0.1-SNAPSHOT/ejb-module.jar#default");
+        factoryBean.setExpectedType(EntityManagerFactory.class);
+        return factoryBean;
     }
 
+    @Bean
+    public EntityManagerFactory entityManagerFactory(JndiObjectFactoryBean emfLookup) throws IllegalArgumentException, NamingException {
+        return (EntityManagerFactory) emfLookup.getObject();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
 }
